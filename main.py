6fcd91c257e04565862cc89f250d83b37a17b75c
Nimansky <nimansky@outlook.de>
2020-04-25 20:01:56 +0200

    Updated main.py
    Finished base functionality:
    -User can input desired amount of digits after comma
    -Program will calculate Pi exactly up to that amount of digits

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-digits = input("Number of decimal places of Pi to be calculated: ")
+import math
+from decimal import *
 
+#let user decide on how many decimal places of pi to display
+digits = int(input("Number of decimal places of Pi to be calculated: "))  
+
+#calculate pi to the n-th digit using the BBP-formula
 def calc_pi(counter):
-    if counter > 0:
-        newnum = ((-1)**counter)/((2*counter)+1)
-        total = newnum + calc_pi(counter-1)
+    return sum(1/Decimal(16)**k * (Decimal(4)/(8*k+1) - Decimal(2)/(8*k+4) - Decimal(1)/(8*k+5) - Decimal(1)/(8*k+6)) for k in range(counter))
         
-#def display_pi(places):
- #   print
-while true:
-    calc_pi(digits)
-    
+#set the precision of the decimals according to the desired number of decimal places;
+#+3 because the formula seems to get every digit right except for the last 3
+getcontext().prec = digits+3
+
+#cut the last 3 extra digits
+#also +2 so as not to include "3." in our x amount of digits
+print(str(calc_pi(digits))[0:digits+2])
